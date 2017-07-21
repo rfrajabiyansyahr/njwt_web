77498ef32fbcbcf889ed0aefdc26d77b9efe41d2
rfrajabiyansyahr <rfrajabiyansyahr@gmail.com>
2017-07-21 15:21:48 +0700

    re-Initialized on Ubuntu 16.04 LTS

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,27 @@
-var njwtApp = angular.module('njwtApp', ['ngMaterial']);
+var njwtApp = angular.module('njwtApp', ['ngMaterial','ui.router']);
 
-njwtApp.controller('njwtCtrl', ['$scope', '$http',function($scope, $http){
+njwtApp.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/login');
+
+    $stateProvider
+        .state('signup', {
+            url: '/signup',
+            templateUrl: '../../views/signup.html',
+            controller: 'signUpCtrl'
+        })
+        .state('login', {
+            url: '/login',
+            templateUrl: '../../views/login.html',
+            controller: 'loginCtrl'
+        })
+        .state('home', {
+            url: '/home',
+            templateUrl: '../../views/home.html'
+        })
+});
+
+njwtApp.controller('signUpCtrl', ['$scope', '$http',function($scope, $http){
     $scope.user = {
         role: 'Administrator'
     };
@@ -19,6 +40,24 @@ njwtApp.controller('njwtCtrl', ['$scope', '$http',function($scope, $http){
             console.log('a new user successfully created');
         }), function(error){
             console.log('failed to create a new user');
+        }
+    }
+}]);
+
+njwtApp.controller('loginCtrl', ['$scope', '$http','$state', function($scope, $http, $state){
+    $scope.user = {};
+    
+    $scope.login = function(){
+        $http({
+            method: 'POST',
+            url: 'http://localhost:8080/api/authenticate',
+            data: $scope.user
+        }).then(function(success){
+            $scope.user = {};
+            $scope.token = success.data.token;
+            $state.transitionTo('home');
+        }),function (error){
+                console.log(error, 'Cannot POST the Data');
         }
     }
 }]);
